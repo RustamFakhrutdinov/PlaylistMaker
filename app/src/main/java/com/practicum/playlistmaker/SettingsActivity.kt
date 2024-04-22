ac5e871603313d8b363821a20f187f28918e9ef8
RustamFakhrutdinov <rustikfah@yandex.ru>
2024-04-23 02:07:23 +0300

    Выполнил все дз 9 спринта

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,55 @@
 package com.practicum.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val shareButton = findViewById<Button>(R.id.share)
+        val supportButton = findViewById<Button>(R.id.support)
+        val userAgreementButton = findViewById<Button>(R.id.user_agreement)
+        val backButton = findViewById<Button>(R.id.arrowBack)
+
+        backButton.setOnClickListener {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
+
+
+        shareButton.setOnClickListener {
+            val message = resources.getString(R.string.message_to_share_with_messegers)
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = resources.getString(R.string.type_for_all_messengers)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+
+        supportButton.setOnClickListener {
+            val subject = resources.getString(R.string.subject_for_email)
+            val message = resources.getString(R.string.message_to_share_with_email)
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.data = Uri.parse(resources.getString(R.string.mailto))
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(resources.getString(R.string.my_email)))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            supportIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(supportIntent)
+        }
+
+        userAgreementButton.setOnClickListener {
+            val url = resources.getString(R.string.url_for_user_agreement)
+            val webpage: Uri = Uri.parse(url)
+            val userAgreementIntent = Intent(Intent.ACTION_VIEW, webpage)
+            startActivity(userAgreementIntent)
+
+        }
     }
+
+
 }

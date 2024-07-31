1d2bb96be1b7e0af28224931128737d76a87b68d
RustamFakhrutdinov <rustikfah@yandex.ru>
2024-07-31 23:55:22 +0300

    Начал делать 13 спринт: Сверстал экран плееера, кроме описания трека

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,5 +49,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.10")
+    implementation ("androidx.constraintlayout:constraintlayout:2.0.0")
+
 
 }
